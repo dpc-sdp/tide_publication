606b59c7bfdb4d4ed92cf4f82dcbcfced6893eae
Yeni Atencio <yeni.atencio@dpc.vic.gov.au>
2023-08-31 17:16:05 +1000

    clean up

--- src/TidePublicationOperation.php
+++ src/TidePublicationOperation.php
@@ -37,47 +37,6 @@ class TidePublicationOperation {
   const PUBLICATION_AUTHORS_FIELD_STORAGE = 'field.storage.node.field_publication_authors';
 
   /**
-   * Update field_publication_authors to use new vocabulary.
-   *
-   * @param \Drupal\Core\Config\Config $config
-   *   The config object.
-   * @param string $vocabulary
-   *   The new vocabulary.
-   */
-  public function updateFieldPublicationAuthorsVocabulary(Config $config, string $vocabulary) {
-    // Creates vocabulary if doesn't exist.
-    $vocabularies = Vocabulary::loadMultiple();
-    if (!isset($vocabularies[$vocabulary])) {
-      $this->createVocabulary($vocabulary);
-    }
-
-    $field = FieldConfig::loadByName('node', self::BUNDLE, self::PUBLICATION_AUTHORS_FIELD_NAME);
-    if ($field->get('field_type') === self::ENTITY_REFERENCE_FIELD_TYPE) {
-      $new_field = $field->toArray();
-      $new_field['field_type'] = self::ENTITY_REFERENCE_FIELD_TYPE;
-      $new_field['description'] = self::PUBLICATION_AUTHORS_FIELD_DESCRIPTION;
-      $new_field['dependencies'] = [
-        'config' => [
-          'field.storage.' . $config->get('id'),
-          'node.type.' . self::BUNDLE,
-          'taxonomy.vocabulary.' . $vocabulary,
-        ],
-      ];
-      $new_field['settings'] = [
-        'handler_settings' => [
-          'target_bundles' => [
-            'department' => $vocabulary,
-          ],
-        ],
-      ];
-      $new_field = FieldConfig::create($new_field);
-      $new_field->original = $field;
-      $new_field->enforceIsNew(FALSE);
-      $new_field->save();
-    }
-  }
-
-  /**
    * Creates a new vocabulary.
    *
    * @param string $vocabulary
