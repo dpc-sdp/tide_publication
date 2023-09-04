307b1187a9b7cc2e2a470f4c7e10da03665e18df
Yeni Atencio <yeni.atencio@dpc.vic.gov.au>
2023-09-04 12:28:36 +1000

    removed const

--- src/TidePublicationOperation.php
+++ src/TidePublicationOperation.php
@@ -10,31 +10,6 @@ use Drupal\taxonomy\Entity\Vocabulary;
 class TidePublicationOperation {
 
   /**
-   * The bundle name.
-   */
-  const BUNDLE = 'publication';
-
-  /**
-   * The entity reference field type.
-   */
-  const ENTITY_REFERENCE_FIELD_TYPE = 'entity_reference';
-
-  /**
-   * The publication authors field name.
-   */
-  const PUBLICATION_AUTHORS_FIELD_NAME = 'field_publication_authors';
-
-  /**
-   * The publication authors field description.
-   */
-  const PUBLICATION_AUTHORS_FIELD_DESCRIPTION = 'Start typing to choose one publication author.';
-
-  /**
-   * The publication authors field storage.
-   */
-  const PUBLICATION_AUTHORS_FIELD_STORAGE = 'field.storage.node.field_publication_authors';
-
-  /**
    * Creates a new vocabulary.
    *
    * @param string $vocabulary
